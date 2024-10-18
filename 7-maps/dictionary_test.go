f460bc202418e0b06a62d07ab73f7431309e43c7
Nick Williams <git@nickawilliams.com>
2024-10-18 11:43:48 -0700

    test(7-maps): refactored tests to include error checking

--- 7-maps/dictionary_test.go
+++ 7-maps/dictionary_test.go
@@ -5,10 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dict := Dictionary{"test": "this is just a test"}
 
-	got := dict.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dict.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dict.Search("unknown")
+		want := "could  not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected an error")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
