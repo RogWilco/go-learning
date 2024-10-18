d2790fa648f22112f89795e30b1bf1183e976976
Nick Williams <git@nickawilliams.com>
2024-10-17 17:40:15 -0700

    feat(1-hello): added additional Swedish and French support

--- 1-hello/hello.go
+++ 1-hello/hello.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const frenchHelloPrefix = "Bonjour, "
 const spanishHelloPrefix = "Hola, "
+const swedishHelloPrefix = "Hej, "
 const suffix = "!"
 
 func Hello(name, language string) string {
@@ -11,8 +13,16 @@ func Hello(name, language string) string {
 		name = "World"
 	}
 
+	if language == "French" {
+		return frenchHelloPrefix + name + suffix
+	}
+
 	if language == "Spanish" {
 		return spanishHelloPrefix + name + suffix
+	}
+
+	if language == "Swedish" {
+		return swedishHelloPrefix + name + suffix
 	}
 
 	return englishHelloPrefix + name + suffix
