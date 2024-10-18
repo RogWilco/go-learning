15c0bb7316be24cf8ae588dd7cfcfefce4413aab
Nick Williams <git@nickawilliams.com>
2024-10-17 17:28:10 -0700

    test(1-hello): expanded tests to specify a language, defaulting to English if the passed language isn't found

--- 1-hello/hello_test.go
+++ 1-hello/hello_test.go
@@ -4,18 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Nick")
+		got := Hello("Nick", "")
 		want := "Hello, Nick!"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World!"
 
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Nikito", "Spanish")
+		want := "Hola, Nikito!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
 }
 
 func assertCorrectMessage(t *testing.T, got, want string) {
