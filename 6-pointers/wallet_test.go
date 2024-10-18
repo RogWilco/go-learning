87535d803bde7834726cc6a59566e1f1947bc79e
Nick Williams <git@nickawilliams.com>
2024-10-18 11:14:52 -0700

    test(6-pointers): improved detection of expected error for overdraft test

--- 6-pointers/wallet_test.go
+++ 6-pointers/wallet_test.go
@@ -12,6 +12,18 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertError := func(t testing.TB, got error, want string) {
+		t.Helper()
+
+		if got == nil {
+			t.Fatal("got nil, want error")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
@@ -33,6 +45,7 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 
+		assertError(t, err, "cannot withdraw, insufficient funds")
 		assertBalance(t, wallet, startingBalance)
 
 		if err == nil {
